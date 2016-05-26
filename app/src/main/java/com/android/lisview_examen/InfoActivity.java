bd50fa2804bb80b014b8f516cc227fe5d00a28f9
SirErick <erickhobo98@gmail.com>
2016-05-26 00:07:39 -0400

    FINAL

--- app/src/main/java/com/android/lisview_examen/InfoActivity.java
+++ app/src/main/java/com/android/lisview_examen/InfoActivity.java
@@ -15,34 +15,53 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        ImageView iv=(ImageView)findViewById(R.id.imageView);
+        Drawable drawable1 = this.getResources().getDrawable(R.drawable.mercurio);
+        Drawable drawable2 = this.getResources().getDrawable(R.drawable.venus);
+        Drawable drawable3 = this.getResources().getDrawable(R.drawable.tierra);
+        Drawable drawable4 = this.getResources().getDrawable(R.drawable.marte);
+        Drawable drawable5 = this.getResources().getDrawable(R.drawable.jupiter);
+        Drawable drawable6 = this.getResources().getDrawable(R.drawable.saturno);
+        Drawable drawable7 = this.getResources().getDrawable(R.drawable.urano);
+        Drawable drawable8 = this.getResources().getDrawable(R.drawable.neptuno);
+        Drawable drawable9 = this.getResources().getDrawable(R.drawable.pluton);
         Intent newint = getIntent();
         Posicion = newint.getStringExtra(MainActivity.Posicion);
         num=Integer.parseInt(Posicion);
         if(num==0){
             setMercurio();
+            iv.setImageDrawable(drawable1);
         }
         if(num==1){
             setVenus();
+            iv.setImageDrawable(drawable2);
         }
         if(num==2){
             setTierra();
+            iv.setImageDrawable(drawable3);
         }
         if(num==3){
             setMarte();
+            iv.setImageDrawable(drawable4);
         }if(num==4){
             setJupiter();
+            iv.setImageDrawable(drawable5);
         }
         if(num==5){
             setSaturno();
+            iv.setImageDrawable(drawable6);
         }
         if(num==6){
             setUrano();
+            iv.setImageDrawable(drawable7);
         }
         if(num==7){
             setNeptuno();
+            iv.setImageDrawable(drawable8);
         }
         if(num==8){
             setPluton();
+            iv.setImageDrawable(drawable9);
         }
 
 
@@ -52,7 +71,7 @@ public class InfoActivity extends AppCompatActivity {
 
             TextView quantityTextView = (TextView) findViewById(
                     R.id.textView);
-            quantityTextView.setText("Distancia media al Sol: 57.870.000 Km.\n" +
+            quantityTextView.setText("MERCURIO.\n" +"Distancia media al Sol: 57.870.000 Km.\n" +
                     "Diámetro: 4.850 Km.\n" +
                     "Duración de la traslación: 57 Km./seg. en el afelio\n" +
                     "Distancia máxima a la Tierra: 220 millones de Km.\n" +
@@ -65,13 +84,11 @@ public class InfoActivity extends AppCompatActivity {
 
         TextView quantityTextView = (TextView) findViewById(
                 R.id.textView);
-        quantityTextView.setText("Temperatura Media: 260°C.\n" +
-                "Satélites: ninguno\n" +
+        quantityTextView.setText("VENUS.\n" +"Temperatura Media: 260°C.\n" +
                 "Distancia media al Sol: 107.826.000 Km.\n" +
                 "Diámetro: 12.373 Km.\n" +
                 "Duración de la traslación: 224.7 días.\n" +
                 "Distancia mínima a la Tierra: 42 millones de Km.\n" +
-                "Volumen: 0,9 el de la Tierra\n" +
                 "Período de rotación (día): 30 días\n" +
                 "Gravedad: 0,81 de la Tierra"
         );
@@ -80,7 +97,7 @@ public class InfoActivity extends AppCompatActivity {
 
         TextView quantityTextView = (TextView) findViewById(
                 R.id.textView);
-        quantityTextView.setText("Diámetro de la Tierra en el ecuador: 12.756 Km.\n" +
+        quantityTextView.setText("TIERRA.\n" +"Diámetro de la Tierra en el ecuador: 12.756 Km.\n" +
                 "Circunferencia de la Tierra en el ecuador: 40.076 Km.\n" +
                 "Diámetro de la Tierra de uno a otro polo: 12.713,82 Km.\n" +
                 "Circunferencia de la Tierra en los polos (meridianos): 40.009,152 Km.\n" +
@@ -93,7 +110,7 @@ public class InfoActivity extends AppCompatActivity {
 
         TextView quantityTextView = (TextView) findViewById(
                 R.id.textView);
-        quantityTextView.setText("Satélites: 2\n" +
+        quantityTextView.setText("MARTE.\n" +"Satélites: 2\n" +
                 "Distancia media al Sol: 227,8 millones de Km.\n" +
                 "Recorrido de su órbita: 687 días\n" +
                 "Velocidad orbital: 24,11 km/seg\n" +
@@ -107,12 +124,12 @@ public class InfoActivity extends AppCompatActivity {
 
         TextView quantityTextView = (TextView) findViewById(
                 R.id.textView);
-        quantityTextView.setText("Posición: 778,570000 km.\n" +
+        quantityTextView.setText("JUPITER.\n" +
+                "Posición: 778,570000 km.\n" +
                 "Rotación.- Este planeta gira sobre su propio eje en 9,84 horas terrestres \n" +
                 "Composición.- Su composición es básicamente gaseosa.\n" +
-                "Gravedad.- Su gravedad es de  2,34 veces la de la tierra.\n" +
-                "Atmosfera.- Su atmosfera se encuentra formada principalmente de Hidrogeno y Helio \n" +
-                "Masa.- La masa del planeta júpiter es de 317,9 veces la de la tierra.\n"
+                "Gravedad.- Su gravedad es de  2,34 veces la de la tierra.\n"
+
 
         );
     }
@@ -120,26 +137,24 @@ public class InfoActivity extends AppCompatActivity {
 
         TextView quantityTextView = (TextView) findViewById(
                 R.id.textView);
-        quantityTextView.setText("Volumen: 719 veces el de la Tierra\n" +
+        quantityTextView.setText(
+                "SATURNO.\n" +"Volumen: 719 veces el de la Tierra\n" +
                 "Distancia media al Sol: 1.429.097.400 Km.\n" +
                 "Diámetro ecuatorial: 120.800 Km.\n" +
                 "Diámetro polar: 108.100 Km.\n" +
                 "Periodo de rotación (día): 10 horas, 48 minutos\n" +
                 "Período de traslación (año): 29,5 años\n" +
                 "Gravedad en la superficie: 1,14 de la Tierra\n" +
-                "Velocidad orbital: 9,7 Km./seg\n" +
-                "Velocidad de escape: 37 Km./seg\n" +
-                "Número de satélites: 10\n" +
-                "Temperatura media: -155°e\n" +
-                "Masa (Tierra = 1): 95,3\n" +
-                "Mayor acercamiento a la Tierra: 1.190.914.500 m."
+                "Velocidad orbital: 9,7 Km./seg\n"
+
         );
     }
     public void setUrano() {
 
         TextView quantityTextView = (TextView) findViewById(
                 R.id.textView);
-        quantityTextView.setText("Masa: 8.6810 x 1025 kg.\n" +
+        quantityTextView.setText("URANO.\n" +
+                "Masa: 8.6810 x 1025 kg.\n" +
                 "Volumen: 6.83344 x 1013 km3.\n" +
                 "Densidad: 1.270 g/cm3.\n" +
                 "Diámetro: 51118 km.\n" +
@@ -150,7 +165,7 @@ public class InfoActivity extends AppCompatActivity {
 
         TextView quantityTextView = (TextView) findViewById(
                 R.id.textView);
-        quantityTextView.setText("Volumen: 42 veces el de la Tierra\n" +
+        quantityTextView.setText("NEPTUNO.\n" +"Volumen: 42 veces el de la Tierra\n" +
                 "Distancia al Sol: 4.496.500.000 km\n" +
                 "Diámetro ecuatorial: 44.600 km\n" +
                 "Rotación (día): 16 horas\n" +
@@ -164,7 +179,7 @@ public class InfoActivity extends AppCompatActivity {
 
         TextView quantityTextView = (TextView) findViewById(
                 R.id.textView);
-        quantityTextView.setText("Volumen: 1.3 de la Tierra\n" +
+        quantityTextView.setText("PLUTON.\n" +"Volumen: 1.3 de la Tierra\n" +
                 "Distancia al Sol: 5.906.292.500 Km.\n" +
                 "Diámetro ecuatorial: 14.500 Km.\n" +
                 "Traslación (ano): 248 años\n" +
